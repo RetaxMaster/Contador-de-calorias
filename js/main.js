00c8697901dd20d35c4e14b233bace6a8f7cd259
RetaxMaster <retaxmaster@hotmail.com>
2020-11-15 22:21:28 -0600

    Adicion de validaciones y pusheado a la lista

--- js/main.js
+++ js/main.js
@@ -1,2 +1,58 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+
+  let description = $("#description");
+  let carbs = $("#carbs");
+  let calories = $("#calories");
+  let protein = $("#protein");
+
+  let list = [];
+
+  description.keypress(() => {
+    description.removeClass("is-invalid");
+  });
+
+  calories.keypress(() => {
+    calories.removeClass("is-invalid");
+  });
+
+  carbs.keypress(() => {
+    carbs.removeClass("is-invalid");
+  });
+
+  protein.keypress(() => {
+    protein.removeClass("is-invalid");
+  });
+
+  const validateInputs = () => {
+
+    description.val() ? "" : description.addClass("is-invalid");
+    carbs.val() ? "" : carbs.addClass("is-invalid");
+    calories.val() ? "" : calories.addClass("is-invalid");
+    protein.val() ? "" : protein.addClass("is-invalid");
+
+    if (description.val() && calories.val() && carbs.val() && protein.val())
+      add()
+
+  }
+
+  const add = () => {
+
+    const newItem = {
+      description: description.val(),
+      calories: parseInt(calories.val()),
+      carbs: parseInt(carbs.val()),
+      protein: parseInt(protein.val())
+    }
+
+    list.push(newItem);
+
+  }
+
+  const cleanInputs = () => {
+    description.val("");
+    calories.val("");
+    carbs.val("");
+    protein.val("");
+  }
